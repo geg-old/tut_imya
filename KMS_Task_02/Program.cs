3cb88f716ebbb0bee52bc7dcf4d92416d64e0fc6
geg-old <73823196+geg-old@users.noreply.github.com>
2020-11-19 01:31:57 +0300

    Add files via upload

---
+++ KMS_Task_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KMS_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            Console.WriteLine("Программа вводит текст до тех пор, пока пользователь не введет 'exit'. \n");
+            Console.WriteLine("Введите текст: ");
+
+            do
+            {
+                text = Console.ReadLine();
+            }
+            while (text != "exit");
+
+            Console.WriteLine("\nПрограмма отработала. Нажмите любую кнопку, чтобы продолжить");
+            Console.ReadKey();
+        }
+    }
+}
